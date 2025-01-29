d30579c6190df2d2fc3376cbd08b813fadc46198
Dimas <dimasardykusuma01@gmail.com>
2025-01-29 21:40:27 +0700

    perbarui favicon34

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 });
 
 // Jalankan server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
