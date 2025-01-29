dfe8c03e4390fa241d077a729c38e921a25ddca4
Dimas <dimasardykusuma01@gmail.com>
2025-01-29 15:17:35 +0700

    perbarui favicon22

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const cors = require('cors');
-const login = require('./routes/login')
-const dataKader= require('./routes/dataKader')
-const sebaranWilayah= require('./routes/sebaranWilayah')
-const laporan = require('./routes/laporan')
-const tambahAkun = require('./routes/tambahAkun')
-const dashboard = require('./routes/dashboard')
-const profil = require('./routes/profil')
-const kta = require('./routes/kta')
 const path = require('path');
 
+// Import routes
+const login = require('./routes/login');
+const dataKader = require('./routes/dataKader');
+const sebaranWilayah = require('./routes/sebaranWilayah');
+const laporan = require('./routes/laporan');
+const tambahAkun = require('./routes/tambahAkun');
+const dashboard = require('./routes/dashboard');
+const profil = require('./routes/profil');
+const kta = require('./routes/kta');
 
-// Middleware setup
+const app = express();
+
+// Middleware Content Security Policy (CSP) untuk keamanan
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self'; img-src 'self' data: https://backend-data-kader.vercel.app; connect-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; manifest-src 'self'; media-src 'self'; worker-src 'self';"
+    "default-src 'self'; " +
+    "img-src 'self' data: https://backend-data-kader.vercel.app https://backend-data-kader.vercel.app/favicon.ico; " +
+    "connect-src 'self'; " +
+    "script-src 'self'; " +
+    "style-src 'self' 'unsafe-inline'; " +
+    "font-src 'self'; " +
+    "object-src 'none'; " +
+    "frame-ancestors 'none'; " +
+    "base-uri 'self'; " +
+    "form-action 'self'; " +
+    "manifest-src 'self'; " +
+    "media-src 'self'; " +
+    "worker-src 'self';"
   );
   next();
 });
 
-
+// Middleware setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json()); // Middleware untuk mengurai JSON body dari request
 app.use(cors());
+
+// Menyajikan file statis, termasuk favicon
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
 
 app.use( login)
 app.use( dataKader)
