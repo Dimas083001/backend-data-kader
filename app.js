d1880b7361380ae44f6ca2e098979182e6929a3f
Dimas <dimasardykusuma01@gmail.com>
2025-01-29 21:49:17 +0700

    perbarui favicon35

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config(); // Load environment variables
+const app = express();
 
-// Import routes
-const login = require('./routes/login');
+const checkDb = require('./routes/checkDb');
+// const login = require('./routes/login');
 const dataKader = require('./routes/dataKader');
 const sebaranWilayah = require('./routes/sebaranWilayah');
 const laporan = require('./routes/laporan');
-const tambahAkun = require('./routes/tambahAkun');
-const dashboard = require('./routes/dashboard');
-const profil = require('./routes/profil');
+// const tambahAkun = require('./routes/tambahAkun');
+// const dashboard = require('./routes/dashboard');
+// const profil = require('./routes/profil');
 const kta = require('./routes/kta');
 
-const app = express();
+// Tambahkan middleware untuk route baru
+app.use(checkDb);
+// app.use(login);
+app.use(dataKader);
+app.use(sebaranWilayah);
+// app.use(tambahAkun);
+app.use(laporan);
+// app.use(dashboard);
+// app.use(profil);
+app.use(kta);
 
-// Middleware setup
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json()); // Middleware untuk mengurai JSON body dari request
-app.use(cors());
-
-// Menyajikan file statis
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
-// Register routes dengan prefix yang benar
-app.use('/login', login);
-app.use('/dataKader', dataKader);
-app.use('/sebaranWilayah', sebaranWilayah);
-app.use('/tambahAkun', tambahAkun);
-app.use('/laporan', laporan);
-app.use('/dashboard', dashboard);
-app.use('/profil', profil);
-app.use('/kta', kta);
-
-// Route utama
 app.get('/', (req, res) => {
-  res.send('API is running!');
+  res.send('Aplikasi berjalan dengan benar!');
 });
 
-// Jalankan server
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server berjalan pada port', process.env.PORT || 3000);
 });
-
-module.exports = app;
-
