cea23edcb956bf8aea73ff42a2d2fdbd16ed6bdb
Dimas <dimasardykusuma01@gmail.com>
2025-01-29 19:01:49 +0700

    perbarui favicon26

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-const login = require('./routes/login')
+//const login = require('./routes/login')
 const dataKader= require('./routes/dataKader')
 const sebaranWilayah= require('./routes/sebaranWilayah')
 const laporan = require('./routes/laporan')
-const tambahAkun = require('./routes/tambahAkun')
-const dashboard = require('./routes/dashboard')
-const profil = require('./routes/profil')
+//const tambahAkun = require('./routes/tambahAkun')
+//const dashboard = require('./routes/dashboard')
+//const profil = require('./routes/profil')
 const kta = require('./routes/kta')
 const path = require('path');
 
@@ -29,13 +29,13 @@ app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use( login)
+//app.use( login)
 app.use( dataKader)
 app.use( sebaranWilayah)
-app.use( tambahAkun)
+//app.use( tambahAkun)
 app.use( laporan)
-app.use ( dashboard)
-app.use ( profil)
+//app.use ( dashboard)
+//app.use ( profil)
 app.use ( kta)
 
 app.get("/", (req, res) => {
