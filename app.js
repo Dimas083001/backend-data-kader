483e8640b42235871fee34e88846ddc63f2a81b3
Dimas <dimasardykusuma01@gmail.com>
2025-01-29 11:23:24 +0700

    ubah app.js3

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.get("/", (req, res) => {
   res.send("API is running!");
 });
 
+app.get('/favicon.ico', (req, res) => res.status(204));
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
